a59ea40f403992532a0e20750d63be692472f495
Jas-0528 <jsandhu202@my.bcit.ca>
2024-05-08 08:58:42 -0700

    Refactor: seperate functions into seed database and write JSON file

--- manage.py
+++ manage.py
@@ -5,6 +5,18 @@ import json
 import requests
 import html
 
+def convert_to_json():
+    # Make API request to OpenTDB
+    url = "https://opentdb.com/api.php?amount=50&type=multiple" 
+    response = requests.get(url)
+    data = response.json()["results"]
+    print("Converted to JSON")
+
+    # write out the gotten data to a file
+    with open("data/trivia.json", "w") as outfile:
+        json.dump(data, outfile, indent=4)
+    print("Written to file")
+
 # create database tables
 def create_all():
     db.create_all()
@@ -15,22 +27,12 @@ def drop_all():
     db.drop_all()
     print("All tables dropped")
 
-# add questions to the database from jjson file created from the API
+# add questions to the database from json file created from the API
 def add_questions():
-    # Make an API request to OpenTDB (replace with the correct endpoint and parameters)
-    url = "https://opentdb.com/api.php?amount=50&type=multiple"  #  Get 50 questions
-    response = requests.get(url)
-    data = response.json()
-    print("Converted to JSON")
-
-    # write out the gotten data to a file
-    with open("data/trivia.json", "w") as outfile:
-        json.dump(data, outfile, indent=4)
     # read the questions.json file
     with open("data/trivia.json") as file:
         # Add logic to add questions here
         data = json.load(file)
-        data = data["results"]
         
     #loop though each question and add it to the database
     for question in data:
@@ -52,4 +54,5 @@ if __name__ == "__main__":
     with app.app_context():
         drop_all()
         create_all()
+        convert_to_json()
         add_questions()
