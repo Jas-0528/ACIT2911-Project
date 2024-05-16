998423fb48f904c88f23ea6bf5b9aa79d671fae1
Augustin Nguyen <144770199+yamiap@users.noreply.github.com>
2024-05-16 11:29:06 -0700

    WIP: create User fixture

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,9 +1,18 @@
-from trivia.models import User
+import pytest
+from trivia.models import Question, Quiz, QuizQuestion, User
 
 
-def test_new_user():
-    user = User(username="user2", email="user2@example.com", password="password1")
+@pytest.fixture
+def user():
+    # Basic user fixture
+    user = User(username="drai_29", email="dleon@oilers.ca", password="gooilersgo")
+    return user
+
+
+def test_new_user(user):
+    # Check attributes and password hashing (pass/fail)
+    user = User(username="Lethamyr", email="leth@psyonix.com", password="rokt_l33g")
     assert user.role == "user"
-    assert user.username == "user2"
-    assert user.email == "user2@example.com"
-    assert user.password_hashed != "password1"
+    assert user.username == "Lethamyr"
+    assert user.email == "leth@psyonix.com"
+    assert user.password_hashed != "rokt_l33g"
