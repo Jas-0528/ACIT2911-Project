503f7255244e9fa85d6d38334acb1aaf41961889
lcahayag <lcahayag@my.bcit.ca>
2024-05-16 19:47:57 -0700

    Feat: match homepage style to other page

---
+++ tests/unit/test_login_auth.py
@@ -0,0 +1,17 @@
+from trivia.models import User
+import pytest
+from flask import url_for
+
+def test_login_get():
+    assert 'login.html' != ''
+
+def test_login_post():
+    username = 'user2'
+    email = 'user2@example.com'
+    password = 'Password1'
+    remember = True
+
+    assert username != ''
+    assert email != ''
+    assert password != ''
+    assert remember is True
